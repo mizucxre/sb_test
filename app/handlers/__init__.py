7f057195bc3cfe8a114321748c5981d1da8c984e
mizucxre <119413490+mizucxre@users.noreply.github.com>
2025-11-13 21:01:04 +0500

    Create __init__.py

---
+++ app/handlers/__init__.py
@@ -0,0 +1,9 @@
+from telegram.ext import Application
+
+def register_handlers(application: Application):
+    """Регистрация всех хэндлеров"""
+    from . import client_handlers, admin_handlers, callback_handlers
+    
+    client_handlers.register(application)
+    admin_handlers.register(application)
+    callback_handlers.register(application)
