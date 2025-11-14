42fa89319ac6d2af45a91a3019bc0c6ed3e693c1
mizucxre <119413490+mizucxre@users.noreply.github.com>
2025-11-14 14:45:54 +0500

    Update __init__.py

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,7 +1,6 @@
 import logging
 from .client_handlers import register as register_client_handlers  
 from .callback_handlers import register as register_callback_handlers
-from .text_handler import register as register_text_handler
 
 logger = logging.getLogger(__name__)
 
@@ -13,8 +12,5 @@ def register_handlers(application):
     
     # 2. Callback хэндлеры (inline кнопки)
     register_callback_handlers(application)
-    
-    # 3. Текстовые сообщения
-    register_text_handler(application)
     
     logger.info("✅ Все хэндлеры зарегистрированы")
